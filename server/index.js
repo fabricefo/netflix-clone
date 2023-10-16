3e9f101798c3628ec66cb52c5be0b1df3338afb8
Fabrice Fourel <ffourel@gmail.com>
2023-10-16 12:52:56 +0200

    Fix mispelling

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
-import express from "express"
-import cookieParser
-import cors from "cors"
-import http from "http"
-import mongoose from "mongoose"
-import "dotenv/config" 
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import http from "http";
+import mongoose from "mongoose";
+import "dotenv/config";
 
-consta app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-app.use(cookieParser)
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser);
 
-const port = process.env.PORT || 5000
-const server = http.createServer(app)
+const port = process.env.PORT || 5000;
+const server = http.createServer(app);
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
     console.log("Mongodb connected");
