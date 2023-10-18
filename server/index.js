5894c5fbed37d7859a592f77098963866bfa0458
Fabrice Fourel <ffourel@gmail.com>
2023-10-18 13:19:35 +0200

    Add route to index

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser);
+app.use("/api/v1", routes);
 
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
